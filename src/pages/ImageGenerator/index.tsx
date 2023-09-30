633d1fe91d288b4fdc2476976c555235e6db1c05
Daria <d.al.tarasova@gmail.com>
2023-10-01 00:59:06 +0200

    enter press handler

--- src/pages/ImageGenerator/index.tsx
+++ src/pages/ImageGenerator/index.tsx
@@ -1,17 +1,30 @@
 import apiTokens from "config";
 import defaultImage from "logo.svg";
-import { useRef, useState } from "react";
-import { Button, Header, Input, Image, Container } from "semantic-ui-react";
+import { useEffect, useState } from "react";
+import { Button, Header, Input, Image, Container, Segment, Grid } from "semantic-ui-react";
 
 
 const ImageGenerator = () => {
   const [imageUrl, setImageUrl] = useState<string>("/");
   const [inputValue, setInputValue] = useState<string>('');
-  
+
+  useEffect(() => {
+    const keyHandler = (event: {which: number}) => {
+      if (event.which === 13) {
+        generateResponse();
+      }
+    }
+    window.addEventListener('keydown', keyHandler);
+    return () => {
+      window.removeEventListener('keydown', keyHandler);
+    }
+  })
+
   const generateResponse = async () => {
     if (inputValue === "") {
       return 0;
     }
+    console.log(inputValue);
 
     const response = await fetch (
       "https://api.openai.com/v1/images/generations", {
@@ -25,42 +38,52 @@ const ImageGenerator = () => {
         body:JSON.stringify({
           prompt: inputValue,
           n: 1,
-          size: "256x256",
+          size: "512x512",
         })
       }
     );
-    console.log("fetch asked");
+
     let data = await response.json();
     let data_array = data.data;
     setImageUrl(data_array[0].url);
+
   }
 
-  
-  
+
   return (
     <div>
-      <Container>
-        <Header as='h3'>Here would be Image Generator</Header>
-        {/* {TEXT} */}
-        <div>
-          <Header as='h4'>Please provide a description to an image you would like to create</Header>
+      <Segment>
+        <Container>
+          <Header as='h3'>Here would be Image Generator</Header>
+          {/* {TEXT} */}
           <div>
-          <Input
-            icon={{ name: 'search', circular: true, link: true }}
-            placeholder='Search...'
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
-          />
-          <Button onClick={() => {generateResponse()}}>SUBMIT</Button>
+            <Header as='h4'>Please provide a description to an image you would like to create</Header>
+            <Grid>
+              <Grid.Column width={2}/>
+              <Grid.Column width={10}>
+                <Input
+                icon={{ name: 'search', circular: true, link: true }}
+                placeholder='Search...'
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                fluid
+                />
+              </Grid.Column>
+              <Grid.Column width={2} textAlign="center">
+                <Button fluid onClick={() => {generateResponse()}}>SUBMIT</Button>
+              </Grid.Column>
+            </Grid>
           </div>
-        </div>
-        {/* {IMAGE} */}
-        <p></p>
-        <div>
-          <Image src={imageUrl === "/" ? defaultImage : imageUrl} size='large' centered />
-        </div>
+          {/* {IMAGE} */}
+          <p></p>
+          <div>
+            <Image src={imageUrl === "/" ? defaultImage : imageUrl} size='large' centered />
+          </div>
 
       </Container>
+
+      </Segment>
+      
       
     </div>
   )
