786e1e156e96ec9a62173c518042df20d2ccf88f
Daria <d.al.tarasova@gmail.com>
2023-10-01 00:08:49 +0200

    home page adjusted

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,16 @@
-import { Header } from 'semantic-ui-react'
+import { Header, List, Image } from 'semantic-ui-react'
+import welcomeImage from 'assets/korgi.png'
 
 const Home = () => {
   return (
     <div>
       <Header as='h1' color='grey'>
-        Home
+        Hi there :{'\u0029'}
       </Header>
-    
+      <div style={{padding: '20px'}}>
+        <Header as='h3'>Welcome to the application. Feel free to explore the pages!</Header>
+        <Image src={welcomeImage} size='large' centered/>
+      </div>
     </div>
   )
 }
